268ea67df980ad55f7d7b65f42ec4d2d74b6964b
Valeria Likso <valerijasergievich@gmail.com>
2023-03-20 19:05:47 +0000

    adds test with status 200

--- __tests__/loginController.test.js
+++ __tests__/loginController.test.js
@@ -4,10 +4,12 @@ const { getMockRes, getMockReq } = require("@jest-mock/express");
 
 const { addNewUser } = require("../services/authService");
 const loginController = require("../controllers/auth/loginController");
-
 const mongoServer = new MongoMemoryServer();
+
 beforeAll(async () => {
-  await mongoose.connect(mongoServer.getUri(), {
+  const mongoUri = await mongoServer.getUri();
+
+  await mongoose.connect(mongoUri, {
     dbName: "verifyLOGIN",
   });
 });
@@ -19,7 +21,7 @@ afterAll(async () => {
 describe("test loginController", () => {
   const { res, next } = getMockRes();
 
-  test("test login, response status 200", async () => {
+  test("email@mail.com, password, token, response status 200", async () => {
     const newUser = {
       email: "email@mail.com",
       password: "password",
@@ -32,12 +34,14 @@ describe("test loginController", () => {
     await loginController(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith({
-      token: expect.toBeTruthy(),
-      user: {
-        email: expect.stringContaining("email@mail.com"),
-        password: experst.stringContaining("password"),
-      },
-    });
+    expect(res.json).toHaveBeenCalledWith(
+      expect.objectContaining({
+        token: expect.anything(),
+        user: expect.objectContaining({
+          email: expect.stringContaining("email@mail.com"),
+          subscription: expect.stringContaining("starter"),
+        }),
+      })
+    );
   });
 });
